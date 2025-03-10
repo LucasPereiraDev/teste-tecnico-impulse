5a5056c6e67d6855f3989ed22bd9b0b9a26d66cf
lucaspereiradev <lucassp724@gmail.com>
2025-03-10 06:46:37 -0300

    refactor--refatorado component atencao

--- components/atencao.tsx
+++ components/atencao.tsx
@@ -1,8 +1,11 @@
 export function Atencao() {
   return (
-    <div className="w-screen bg-red-800 p-2">
-      <div className="bg-[url('/atencao.svg')] bg-repeat-x bg-contain w-full h-3 ">
-      </div>
+    //<div className="w-screen bg-red-800 p-2">
+      //<div className="bg-[url('/atencao.svg')] bg-repeat-x bg-contain w-full h-3 ">
+      //</div>
+    //</div>
+    <div className="w-screen bg-red-800 p-1 flex justify-center">
+      <h1 className="text-white font-bold" >🚨 ÚLTIMAS VAGAS DISPONÍVEIS! NÃO PERCA ESSA OPORTUNIDADE ÚNICA 🚨 </h1>
     </div>
   );
 }
