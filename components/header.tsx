f4ee0b172ec4341951321c86b56b10fb98fe3e99
lucaspereiradev <lucassp724@gmail.com>
2025-03-11 18:05:34 -0300

    style--Alterado responsividade e background

--- components/header.tsx
+++ components/header.tsx
@@ -1,12 +1,11 @@
 export function Header() {
   return (
-    <header className="bg-black flex flex-wrap justify-between py-3 px-10 font-serif items-center">
-      <h1 className="text-2xl font-bold text-white font-serif">Unique Imobi</h1>
-      <nav>
-        <ul className="flex flex-wrap text-white text-base gap-6 tracking-wider">
-          <li className="hover:text-gray-400 cursor-pointer">O que você vai aprender</li>
+    <header className="bg-zinc-950 shadow-xl flex flex-wrap justify-between py-3 px-10 font-serif items-center">
+      <h1 className="text-2xl sm:text-4xl font-bold text-white"><span className="text-blue-400 ">Unique </span>Imobi</h1>
+      <nav className="hidden sm:block">
+        <ul className="flex flex-wrap text-white text-base gap-6 tracking-wider font-mono">
+          <li className="hover:text-gray-400 cursor-pointer mr-10">O que você vai aprender</li>
           <li className="hover:text-gray-400 cursor-pointer">Depoimentos</li>
-          <li className="hover:text-gray-400 cursor-pointer">Contato</li>
         </ul>
       </nav>
     </header>
