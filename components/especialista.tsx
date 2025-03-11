0ac1b9cbca642fa41f4fa2614f7ab2af5580ec78
lucaspereiradev <lucassp724@gmail.com>
2025-03-11 18:06:34 -0300

    style--Alterado font-size

--- components/especialista.tsx
+++ components/especialista.tsx
@@ -1,10 +1,10 @@
 export function Especialista () {
   return (
     <section className="mt-12 text-center px-6 flex justify-center flex-col items-center">
-      <h2 className="text-3xl font-bold text-yellow-400">Quem vai te ensinar?</h2>
-      <div className="mt-6 max-w-md bg-gray-800 p-6 rounded-lg shadow-lg">
+      <h2 className="text-xl sm:text-3xl font-bold text-yellow-400 mt-12">Quem vai te ensinar?</h2>
+      <div className="mt-8 max-w-md bg-gray-800 p-6 rounded-lg shadow-lg">
         <img src="/avatar3.jpg" alt="avatar especialista" width={200} height={50} className="rounded-full mx-auto" />
-        <h3 className="text-2xl text-white mt-4">Mateus Pereira</h3>
+        <h3 className="text-xl sm:text-2xl text-white mt-4">Mateus Pereira</h3>
         <p className="text-gray-300 mt-2">Investidor e mentor com mais de 10 anos no mercado imobiliário. Já ajudou mais de 1.000 alunos a alcançarem o sucesso.</p>
       </div>
     </section>
