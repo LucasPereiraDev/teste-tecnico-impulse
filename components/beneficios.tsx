89aa01b8bc744ee6ebb9038b5036230d75c1bba8
lucaspereiradev <lucassp724@gmail.com>
2025-03-11 18:21:01 -0300

    fix--Bug nas imagens

--- components/beneficios.tsx
+++ components/beneficios.tsx
@@ -8,19 +8,19 @@ export function Beneficios() {
         <ul className="space-y-6 text-base sm:text-lg text-white px-6 py-6">
           <h2 className="text-lg sm:text-3xl font-bold text-yellow-400 mb-10 ">Por que participar?</h2>
           <li className="flex gap-3 ">
-            <img src="/group.svg" width={25} height={25} alt="selo check"/> 
+            <img src="/Group.svg" width={25} height={25} alt="selo check"/> 
             Aprenda os segredos do mercado imobiliário
           </li>
           <li className="flex gap-3">
-            <img src="/group.svg" width={25} height={25} alt="selo check"/> 
+            <img src="/Group.svg" width={25} height={25} alt="selo check"/> 
             Técnicas de negociação para fechar grandes negócios
             </li>
           <li className="flex gap-3">
-            <img src="/group.svg" width={25} height={25} alt="selo check"/> 
+            <img src="/Group.svg" width={25} height={25} alt="selo check"/> 
             Estratégias de captação de imóveis lucrativos
           </li>
           <li className="flex gap-3">
-            <img src="/group.svg" width={25} height={25} alt="selo check"/> 
+            <img src="/Group.svg" width={25} height={25} alt="selo check"/> 
           Certificado na conclusão da mentoria
           </li>
         </ul>
