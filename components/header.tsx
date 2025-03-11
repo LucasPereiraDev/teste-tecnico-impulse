9c82ad2d30d60ff9c5f20df9478113040be61403
lucaspereiradev <lucassp724@gmail.com>
2025-03-10 21:12:28 -0300

    style---troca de font-size

--- components/header.tsx
+++ components/header.tsx
@@ -1,9 +1,9 @@
 export function Header() {
   return (
     <header className="bg-black flex flex-wrap justify-between py-3 px-10 font-serif items-center">
-      <h1 className="text-3xl font-bold text-white font-serif">Unique Imobi</h1>
+      <h1 className="text-2xl font-bold text-white font-serif">Unique Imobi</h1>
       <nav>
-        <ul className="flex flex-wrap text-white gap-6 tracking-wider">
+        <ul className="flex flex-wrap text-white text-base gap-6 tracking-wider">
           <li className="hover:text-gray-400 cursor-pointer">O que você vai aprender</li>
           <li className="hover:text-gray-400 cursor-pointer">Depoimentos</li>
           <li className="hover:text-gray-400 cursor-pointer">Contato</li>
