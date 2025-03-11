4f6639ce9df44504b3f689b758738f27bc8a0c2a
lucaspereiradev <lucassp724@gmail.com>
2025-03-10 21:13:57 -0300

    change--alterado estrutura e retirado comentário

--- components/atencao.tsx
+++ components/atencao.tsx
@@ -1,11 +1,7 @@
 export function Atencao() {
   return (
-    //<div className="w-screen bg-red-800 p-2">
-      //<div className="bg-[url('/atencao.svg')] bg-repeat-x bg-contain w-full h-3 ">
-      //</div>
-    //</div>
-    <div className="w-screen bg-red-800 p-1 flex justify-center">
-      <h1 className="text-white font-bold" >🚨 ÚLTIMAS VAGAS DISPONÍVEIS! NÃO PERCA ESSA OPORTUNIDADE ÚNICA 🚨 </h1>
+    <div className="w-screen bg-red-800 flex justify-center max-w-full">
+      <p className="text-white font-bold sm:text-base text-sm text-center" >🚨 ÚLTIMAS VAGAS DISPONÍVEIS! NÃO PERCA ESSA OPORTUNIDADE ÚNICA 🚨 </p>
     </div>
   );
 }
