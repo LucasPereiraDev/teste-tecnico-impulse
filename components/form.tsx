d4dee704b0306b828a6eee5bd30789544b1ebfd9
lucaspereiradev <lucassp724@gmail.com>
2025-03-10 21:09:12 -0300

    change--Troca de cor e melhora na responsividade

--- components/form.tsx
+++ components/form.tsx
@@ -1,7 +1,7 @@
 export function Form() {
   return (
     <div className="mt-8 bg-gray-950 p-6 rounded-lg shadow-lg w-full max-w-md flex flex-col justify-self-center ">
-      <p className="text-center text-gray-300 text-lg mb-4">Preencha com seu melhor e-mail e receba o link para garantir sua vaga!</p>
+      <p className="text-base sm:text-lg text-center text-zinc-300 mb-4">Preencha com seu melhor e-mail e receba o link para garantir sua vaga!</p>
       <form action="" className="flex flex-col">
         <input 
         type="text" 
@@ -10,7 +10,7 @@ export function Form() {
         placeholder="Digite o seu melhor e-mail"
         className="bg-white text-black p-3 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"/>
       </form>
-      <button className="bg-blue-600 text-white font-bold text-lg px-5 py-3 mt-4 rounded-md hover:bg-blue-700 transition-all">
+      <button className="bg-blue-600 text-white font-bold text-base sm:text-lg px-5 py-3 mt-4 rounded-md hover:bg-blue-700 transition-all">
         Quero minha vaga agora!
       </button>
     </div>
